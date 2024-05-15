930053da8c2f80bf52d2cee99c718aeb568263b2
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-15 20:07:57 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -6,7 +6,30 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+    double month[12];
+	for (int i = 0; i < 12; i++) {
+        cout << "¬вед≥ть прибуток за м≥с€ць " << i + 1 << ": ";
+		cin >> month[i];
+	}
+    int maxpricezp = 0;
+	double maxzp = month[0];
+    for (int i = 1; i < 12; i++) {
+        if (month[i] > maxzp)
+        {
+			maxzp = month[i];
+			maxpricezp = i;
+        }
+    }
+	int minpricezp = 0;
+	double minzp = month[0];
+	for (int i = 1; i < 12; i++) {
+		if (month[i] < minzp)
+		{
+			minzp = month[i];
+			minpricezp = i;
+		}
+	}
+	cout << "ћаксимальний прибуток був у м≥с€ц≥ " << maxpricezp + 1 << " та в≥н становив " << maxzp << endl;
     return 0;
 }
 
