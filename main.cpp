62367dce56b1753bc2dde925daeeba33ad56c4b1
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-15 19:40:54 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+	int number[5] = { 1, 2, 3, 4, 5 };
+	cout << "Массив: ";
+	cout << number[0] << " " << number[1] << " " << number[2] << " " << number[3] << " " << number[4] << endl;
+	for (int i = 4; i >= 0; i--) {
+		cout << number[i] << " ";
+    }
     return 0;
 }
 
