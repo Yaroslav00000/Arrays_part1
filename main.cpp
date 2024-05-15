18a00329fb57345cf19483caf40cb5f9c2b34eb5
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-15 19:34:22 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <Windows.h>
-#include <ctime>
+
 using namespace std;
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+	
+	double arr[6];
+	for (int i = 0; i < 6; i++) {
+		cout << "¬вед≥ть прибуток за м≥с€ць " << i+1 << ": ";
+		cin >> arr[i];
+	}
+	double sum = 0;
+	for (int i = 0; i < 6; i++) {
+		sum += arr[i];
+	}
+	cout << "—ума прибутку за п≥вр≥чч€: " << sum << endl;
     return 0;
 }
 
