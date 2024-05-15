291e2dc9ea7cb5c9cec6782e3db1046bf953306b
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-05-15 19:51:05 +0300

    Task complated

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <Windows.h>
-#include <ctime>
+
 using namespace std;
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
+	double arr[5];
+	for (int i = 0; i < 5; i++) {
+		cout << "Введіть довжину сторони " << i + 1 << ": ";
+		cin >> arr[i];
+	}
+	double perumetr = 0;
+	for (int i = 0; i < 5; i++) {
+		perumetr += arr[i];
+	}
+	cout << "Периметр п'ятикутника: " << perumetr << endl;
     return 0;
 }
 
